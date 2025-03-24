ed78e4b32c063de7a4e854947aedde2dc4071842
samncorn <samuel41@illinois.edu>
2025-03-24 14:18:33 -0500

    minor refactor

--- src/dbscan.jl
+++ src/dbscan.jl
@@ -9,12 +9,16 @@ function DBSCAN(points, r, min_pts; leafsize = 25, reorder = true, n_chunks = 1,
     labels = zeros(Int, N)
 
     mergers = Vector{Vector{Tuple{Int, Int}}}(undef, n_chunks)
-    chunksize = ceil(Int, N / n_chunks)
-    chunks = collect(Iterators.partition(eachindex(points), chunksize))
+    # chunksize = ceil(Int, N / n_chunks)
+    # chunks = collect(Iterators.partition(eachindex(points), chunksize))
 
-    Threads.@threads for i in 1:n_chunks
-        points_idx = chunks[i]
-        mergers[i] = _dbscan_kernel!(labels, tree, points, points_idx, r, min_pts, max_pts)
+    Threads.@threads for i_chunk in 1:n_chunks
+        idxs = i_chunk:n_chunks:length(points)
+        for i in idxs
+            in_chunk = x -> x ∈ idxs
+            pts_view = view(points, idxs)
+            mergers[i] = _dbscan_kernel!(labels, tree, pts_view, in_chunk, r, min_pts, max_pts)
+        end
     end 
     for m in mergers
         locks = [ReentrantLock() for _ in labels]
@@ -27,7 +31,7 @@ function DBSCAN(points, r, min_pts; leafsize = 25, reorder = true, n_chunks = 1,
     return collect_labels(labels)
 end
 
-function _dbscan_kernel!(labels, tree, points, points_idx, r, min_pts, max_pts)
+function _dbscan_kernel!(labels, tree, points, in_chunk, r, min_pts, max_pts)
     mergers = Tuple{Int, Int}[] # save edges that cross a chunk boundary for post processing
     neighborhood = Int[]
     for i in points_idx
@@ -43,12 +47,11 @@ function _dbscan_kernel!(labels, tree, points, points_idx, r, min_pts, max_pts)
         end
 
         for j in neighborhood
-            i <= j && continue
-            if !in(j, points_idx)
+            if !in_chunk(j) && i < j
                 push!(mergers, (i, j))
-                continue # only assign
+            else
+                join_labels!(labels, i, j)
             end
-            join_labels!(labels, i, j)
         end
     end
     return mergers
